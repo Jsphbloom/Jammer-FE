a7a09692f2ad6bf21812f63ce07d50f4e3c7098b
Joseph Bloom <jsphbloom@gmail.com>
2025-04-29 11:39:00 -0600

    home page displays with user buttons that route to schedules

---
+++ src/UserScheduleContainer/UserScheduleContainer.jsx
@@ -0,0 +1,9 @@
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './UserScheduleContainer.css'
+
+function UserScheduleContainer() {
+    <p>user schedule container!</p>
+}
+
+export default UserScheduleContainer
---
+++ src/UsersContainer/UsersContainer.jsx
@@ -0,0 +1,9 @@
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './UsersContainer.css'
+
+function UsersContainer() {
+    <p>users container!</p>
+}
+
+export default UsersContainer
